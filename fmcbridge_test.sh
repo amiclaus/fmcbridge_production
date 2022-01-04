5a2cc6a25a72c1d38fce4ac1ce16fa8bee5e7dd8
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-01-04 17:43:03 +0200

    fmcbridge: add checks for all spi/i2c devices
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- fmcbridge_test.sh
+++ fmcbridge_test.sh
@@ -9,13 +9,40 @@ I2C2_ADDRESS=83100000
 GPIO_FIRST=`ls -l /sys/class/gpio/ | grep " gpiochip" | grep "$GPIO_ADDRESS" | grep -Eo '[0-9]+$'`
 
 SPI1_DEVICE=`ls -l /sys/bus/iio/devices/ | grep "$SPI1_ADDRESS" | grep -Eo '[0-9]+$'`
+SPI2_DEVICE=`ls -l /sys/bus/iio/devices/ | grep "$SPI2_ADDRESS" | grep -Eo '[0-9]+$'`
+I2C1_DEVICE=`ls -l /sys/bus/iio/devices/ | grep "$SPI1_ADDRESS" | grep -Eo '[0-9]+$'`
+I2C2_DEVICE=`ls -l /sys/bus/iio/devices/ | grep "$SPI2_ADDRESS" | grep -Eo '[0-9]+$'`
 
 if [ -z $SPI1_DEVICE ]; then
-	echo "SPI device 1 not found"
+	echo "AD5761_SPI1 not found."
 	exit 1
 else
 	SPI1_DEVICE="iio:device${SPI1_DEVICE}"
 	echo "SPI device 1: ${SPI1_DEVICE}"
+fi
+
+if [ -z $SPI2_DEVICE ]; then
+	echo "AD5761_SPI12not found."
+	exit 1
+else
+	SPI2_DEVICE="iio:device${SPI2_DEVICE}"
+	echo "SPI device 2: ${SPI2_DEVICE}"
+fi
+
+if [ -z $I2C1_DEVICE ]; then
+	echo "AD7291_I2C1 not found."
+	exit 1
+else
+	I2C1_DEVICE="iio:device${I2C1_DEVICE}"
+	echo "I2C device 2: ${I2C1_DEVICE}"
+fi
+
+if [ -z $I2C2_DEVICE ]; then
+	echo "AD7291_I2C2 not found."
+	exit 1
+else
+	I2C2_DEVICE="iio:device${I2C2_DEVICE}"
+	echo "I2C device 2: ${I2C2_DEVICE}"
 fi
 
 if [ -z $GPIO_FIRST ]; then
