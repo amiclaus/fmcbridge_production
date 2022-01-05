64e2247f6422f6b16a954617241582c8ac69b104
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-01-05 13:56:26 +0200

    fmcbridge: add SPI1 CS GPIO testing
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- fmcbridge_test.sh
+++ fmcbridge_test.sh
@@ -18,7 +18,7 @@ if [ -z $SPI1_DEVICE ]; then
 	exit 1
 else
 	SPI1_DEVICE="iio:device${SPI1_DEVICE}"
-	echo "SPI device 1: ${SPI1_DEVICE}"
+	echo "SPI device 1 found: ${SPI1_DEVICE}"
 fi
 
 if [ -z $SPI2_DEVICE ]; then
@@ -26,7 +26,7 @@ if [ -z $SPI2_DEVICE ]; then
 	exit 1
 else
 	SPI2_DEVICE="iio:device${SPI2_DEVICE}"
-	echo "SPI device 2: ${SPI2_DEVICE}"
+	echo "SPI device 2 found: ${SPI2_DEVICE}"
 fi
 
 if [ -z $I2C1_DEVICE ]; then
@@ -34,7 +34,7 @@ if [ -z $I2C1_DEVICE ]; then
 	exit 1
 else
 	I2C1_DEVICE="iio:device${I2C1_DEVICE}"
-	echo "I2C device 2: ${I2C1_DEVICE}"
+	echo "I2C device 2 found: ${I2C1_DEVICE}"
 fi
 
 if [ -z $I2C2_DEVICE ]; then
@@ -42,7 +42,7 @@ if [ -z $I2C2_DEVICE ]; then
 	exit 1
 else
 	I2C2_DEVICE="iio:device${I2C2_DEVICE}"
-	echo "I2C device 2: ${I2C2_DEVICE}"
+	echo "I2C device 2 found: ${I2C2_DEVICE}"
 fi
 
 if [ -z $GPIO_FIRST ]; then
@@ -52,16 +52,18 @@ else
 	echo "GPIO initial offset found: $GPIO_FIRST"
 fi
 
-GPIO_LAST=$(($GPIO_FIRST + 21))
+GPIO_LAST=$(($GPIO_FIRST + 20))
 
+echo "Initializing GPIOs"
 for ((i=$GPIO_FIRST;i<=$GPIO_LAST;i++))
 do
 	echo "$i" > /sys/class/gpio/export 2>&1
-done	
+done
+echo "GPIO initialization done."
 
 echo "SPI device 1 scale ${SPI1_SCALE}"
 
-echo "~~~~~~~~~Start testing ADC2~~~~~~~~~~~"
+echo "\n~~~~~~~~~Start testing ADC2~~~~~~~~~~~\n"
 for ((i=0;i<=5;i++))
 do
 	GPIO=$(($GPIO_FIRST+$i))
@@ -78,9 +80,46 @@ do
 	echo `cat /sys/bus/iio/devices/${I2C2_DEVICE}/in_voltage${i}_raw`
 done
 
+echo "\n~~~~~~~~~Start testing DAC1~~~~~~~~~~~\n"
 #Test DAC1
-
 echo "Writing raw value 2000 to DAC1"
 echo 2000 > /sys/bus/iio/devices/${SPI1_DEVICE}/out_voltage_raw
 echo "Reading raw value from DAC1:"
 echo `cat /sys/bus/iio/devices/${SPI1_DEVICE}/out_voltage_raw`
+
+echo "\n~~~~~~~~~Start testing DAC2~~~~~~~~~~~\n"
+#Test DAC2
+echo "Writing raw value 2000 to DAC2"
+echo 2000 > /sys/bus/iio/devices/${SPI2_DEVICE}/out_voltage_raw
+echo "Reading raw value from DAC2:"
+echo `cat /sys/bus/iio/devices/${SPI2_DEVICE}/out_voltage_raw`
+
+echo "\n~~~~~~~~~Start testing SPI1 GPIOS~~~~~~~~~~~\n"
+SPI1_GPIO_FIRST=$(($GPIO_FIRST + 8))
+GPIO_INPUT=$(($GPIO_FIRST + 4))
+
+echo in > /sys/class/gpio/gpio$GPIO_INPUT/direction
+
+GPIO0=$(($GPIO_FIRST))
+GPIO1=$(($GPIO_FIRST+1))
+GPIO2=$(($GPIO_FIRST+2))
+for ((i=0;i<=8;i++))
+do
+	A0=$((($i>>0) & 1))
+	A1=$((($i>>1) & 1))
+	A2=$((($i>>2) & 1))
+	echo "Testing SPI1_CS${i}"
+	echo "A2:${A2} A2:${A1} A0:${A0}"
+	echo $A2 > /sys/class/gpio/gpio$GPIO0/value
+	echo $A1 > /sys/class/gpio/gpio$GPIO1/value
+	echo $A0 > /sys/class/gpio/gpio$GPIO2/value
+	SPI1_CS_GPIO=$(($SPI1_GPIO_FIRST + $i))
+	echo "SPI1_CS${i} set high"
+	echo 1 > /sys/class/gpio/gpio$SPI1_CS_GPIO/value
+	echo "Reading GPIO INPUT:"
+	echo `/sys/class/gpio/gpio$GPIO_INPUT/value`
+	echo "SPI1_CS${i} set low"
+	echo 1 > /sys/class/gpio/gpio$SPI1_CS_GPIO/value
+	echo "Reading GPIO INPUT:"
+	echo `/sys/class/gpio/gpio$GPIO_INPUT/value`
+done
