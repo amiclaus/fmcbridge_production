eff430fd7403dc094cbbedd2504bb233ac2a7b05
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-01-11 17:15:08 +0200

    setup_env: sync with talise script
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- setup_env.sh
+++ setup_env.sh
@@ -7,7 +7,13 @@ set -e
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
-BOARD="fmcbridge"
+source $SCRIPT_DIR/lib/utils.sh
+
+INIT_PINS_SCRIPT="$SCRIPT_DIR"/init.sh
+
+#----------------------------------#
+# Functions section                #
+#----------------------------------#
 
 sudo_required() {
 	type sudo &> /dev/null || {
@@ -33,6 +39,92 @@ setup_apt_install_prereqs() {
 		python3-pytest python3-libiio python3-scapy python3-scipy
 	/etc/init.d/htpdate restart
 	EOF
+}
+
+__common_build_tool() {
+	local c_files
+	mkdir -p work
+
+	for c_file in $tool_c ; do
+		cp -f "src/$c_file" "work/$c_file"
+		c_files="$c_files work/$c_file"
+	done
+	gcc $c_files -o "work/$tool" $cflags $ldflags
+}
+
+__download_github_common() {
+	local gh_prj="$1"
+	mkdir -p work
+
+	local ver=$(get_latest_release analogdevicesinc/$gh_prj)
+	[ -d work/$gh_prj ] || {
+		if [ -z "$ver" ] ; then
+			echo_red "Could not get $gh_prj release tag; cloning repo"
+			git clone https://github.com/analogdevicesinc/$gh_prj work/$gh_prj
+		else
+			echo_green "Using latest released version '$ver' of '$gh_prj'"
+			download_and_unzip_to "https://github.com/analogdevicesinc/$gh_prj/archive/${ver}.zip" "work" || {
+				echo_red "Could not download $gh_prj..."
+				exit 1
+			}
+			mv -f work/${gh_prj}* work/$gh_prj
+		fi
+	}
+}
+
+setup_libiio() {
+	[ ! -d "work/libiio" ] || return 0
+
+	__download_github_common libiio
+	__download_github_common libad9361-iio
+
+	pushd work
+	mkdir -p libiio/build
+	pushd libiio/build
+
+	cmake ..
+	make -j3
+	sudo make install
+
+	popd
+
+	pushd libad9361-iio
+
+	cmake ./CMakeLists.txt
+	make -j3
+	sudo make install
+	sudo ldconfig
+
+	popd
+	popd
+}
+
+setup_adm1266() {
+	[ ! -d "src/adm1266" ] || return 0
+
+	pushd src
+	pushd adm1266
+
+	make all
+
+	popd
+	popd
+}
+
+setup_pyadi-iio() {
+	[ ! -d "work/pyadi-iio" ] || return 0
+
+	__download_github_common pyadi-iio
+	#Set python3 as default
+	sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
+	sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.7 2
+
+	pushd work
+	pushd pyadi-iio
+	git checkout som-testing-fmcomms8
+
+	popd
+	popd
 }
 
 setup_write_autostart_config() {
@@ -61,7 +153,7 @@ Version=0.9.4
 Type=Application
 Name=test-jig-tool
 Comment=test-jig-tool
-Exec=sudo xfce4-terminal --font="DejaVu Sans Mono $font_size" --fullscreen --hide-borders --hide-scrollbar --hide-menubar -x $SCRIPT_DIR/production_${BOARD}.sh
+Exec=sudo xfce4-terminal --font="DejaVu Sans Mono $font_size" --fullscreen --hide-borders --hide-scrollbar --hide-menubar -x $SCRIPT_DIR/production_fmcbridge.sh
 OnlyShowIn=XFCE;LXDE
 StartupNotify=false
 Terminal=false
@@ -72,6 +164,26 @@ Hidden=false
 		sudo ufw enable
 		sudo ufw allow ssh
 	fi
+
+	mkdir -p "$HOME/.ssh"
+	cat "$SCRIPT_DIR/config/jig_id.pub" >> "$HOME/.ssh/authorized_keys"
+	sudo chown "$USER.$USER" "$HOME/.ssh/authorized_keys"
+	chmod 0600 "$HOME/.ssh/authorized_keys"
+
+	sudo chown "$USER.$USER" "$SCRIPT_DIR/config/jig_id"
+	chmod 0600 "$SCRIPT_DIR/config/jig_id"
+	cat > $autostart_path/call-home.desktop <<-EOF
+[Desktop Entry]
+Encoding=UTF-8
+Version=0.9.4
+Type=Application
+Name=call-home
+Comment=call-home
+Exec=/bin/bash $SCRIPT_DIR/call_home
+StartupNotify=false
+Terminal=false
+Hidden=false
+	EOF
 
 	cat > $autostart_path/auto-save-logs.desktop <<-EOF
 [Desktop Entry]
@@ -88,6 +200,205 @@ Hidden=false
 
 }
 
-setup_apt_install_prereqs
+xfconf_has_cap() {
+	type xfconf-query &> /dev/null || return 1
+	if xfconf-query -l | grep -q xfce4-power-manager ; then
+		return 0
+	fi
+	return 1
+}
 
-setup_write_autostart_config
+setup_xfce4_power_manager_settings() {
+	local pm_sett="/xfce4-power-manager/blank-on-ac=0
+		/xfce4-power-manager/blank-on-battery=0
+		/xfce4-power-manager/brightness-switch=0
+		/xfce4-power-manager/brightness-switch-restore-on-exit=1
+		/xfce4-power-manager/dpms-enabled=false
+		/xfce4-power-manager/dpms-on-ac-off=60
+		/xfce4-power-manager/dpms-on-ac-sleep=20
+		/xfce4-power-manager/dpms-on-battery-off=30
+		/xfce4-power-manager/dpms-on-battery-sleep=15
+		/xfce4-power-manager/lock-screen-suspend-hibernate=true
+		/xfce4-power-manager/logind-handle-lid-switch=false
+		/xfce4-power-manager/power-button-action=4
+		/xfce4-power-manager/show-panel-label=0"
+	xfconf_has_cap xfce4-power-manager || return 0
+	for sett in $pm_sett ; do
+		local key="$(echo $sett | cut -d'=' -f1)"
+		local val="$(echo $sett | cut -d'=' -f2)"
+		xfconf-query -c xfce4-power-manager -p $key -s $val
+	done
+}
+
+setup_disable_sudo_passwd() {
+	sudo_required
+	sudo -s <<-EOF
+		if ! grep -q $USER /etc/sudoers ; then
+			echo "$USER	ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+		fi
+	EOF
+}
+
+setup_thunar_volman() {
+	local configs="/autobrowse/enabled=false
+		/autoburn/enabled=false
+		/autoipod/enabled=false
+		/autokeyboard/enabled=false
+		/automount-drives/enabled=true
+		/automount-media/enabled=true
+		/automouse/enabled=false
+		/autoopen/enabled=false
+		/autophoto/enabled=false
+		/autoplay-audio-cds/enabled=false
+		/autoplay-video-cds/enabled=false
+		/autoprinter/enabled=false
+		/autorun/enabled=false
+		/autotablet/enabled=false"
+	xfconf_has_cap thunar-volman || return 0
+	for sett in $configs ; do
+		local key="$(echo $sett | cut -d'=' -f1)"
+		local val="$(echo $sett | cut -d'=' -f2)"
+		xfconf-query -c thunar-volman -p $key -s $val
+	done
+}
+
+setup_disable_lxde_automount() {
+	[ -d "$HOME/.config/pcmanfm" ] || return 0
+
+	pushd "$HOME/.config/pcmanfm/"
+	for cfg_file in $(find . -name pcmanfm.conf) ; do
+		sed 's/autorun=1/autorun=0/g' -i $cfg_file
+	done
+
+	popd
+}
+
+setup_pi_boot_config() {
+
+	[ -f /boot/config.txt ] || return 0
+
+	local tmp=$(mktemp)
+	cat >> $tmp <<-EOF
+# --- added by setup_env.sh
+hdmi_force_hotplug=1
+hdmi_group=2
+hdmi_mode=87
+hdmi_cvt=800 480 60 6 0 0 0
+hdmi_drive=1
+max_usb_current=1
+
+dtoverlay=pi3-disable-wifi
+dtoverlay=pi3-disable-bt
+# --- end setup_env.sh
+	EOF
+
+	sudo -s <<-EOF
+		sed -i -e "/^# --- added by setup_env.sh/,/^# --- end setup_env.sh/d" /boot/config.txt
+		cat $tmp >> /boot/config.txt
+		rm -f $tmp
+	EOF
+}
+
+setup_disable_pi_screen_blanking() {
+	local pi_serial="$(pi_serial)"
+	[ -n "$pi_serial" ] || return 0
+
+	local tmp=$(mktemp)
+	cat >> $tmp <<-EOF
+# --- added by setup_env.sh
+[SeatDefaults]
+xserver-command=X -s 0 -dpms
+# --- end setup_env.sh
+	EOF
+
+	sudo -s <<-EOF
+		sed -i -e "/^# --- added by setup_env.sh/,/^# --- end setup_env.sh/d" /etc/lightdm/lightdm.conf
+		cat $tmp >> /etc/lightdm/lightdm.conf
+	EOF
+}
+
+setup_raspi_config() {
+	sudo -s <<-EOF
+		if type raspi-config &> /dev/null ; then
+			raspi-config nonint do_ssh 0 # enable SSH
+		fi
+	EOF
+}
+
+setup_misc_profile_cleanup() {
+	touch $HOME/.hushlogin # tell login to not print system info
+	sudo -s <<-EOF
+		# Kind of hacky, but it works ; this will remove the warnings
+		# about the SSH password & Wi-Fi on console login
+		[ ! -f /etc/profile.d/sshpwd.sh ] || echo -n > /etc/profile.d/sshpwd.sh
+		[ ! -f /etc/profile.d/wifi-country.sh ] || echo -n > /etc/profile.d/wifi-country.sh
+		[ ! -f /etc/xdg/lxsession/LXDE-pi/sshpwd.sh ] || echo -n > /etc/xdg/lxsession/LXDE-pi/sshpwd.sh
+	EOF
+}
+
+
+setup_bashrc_update() {
+	sed -i -e "/^# --- added by setup_env.sh/,/^# --- end setup_env.sh/d" "$HOME/.bashrc"
+
+	cat >> $HOME/.bashrc <<-EOF
+# --- added by setup_env.sh
+export SCRIPT_DIR="$SCRIPT_DIR"
+source "$SCRIPT_DIR/vars.sh"
+export IIOD_REMOTE=analog.local
+export PATH=/usr/lib/:$PATH
+# --- end setup_env.sh
+	EOF
+}
+
+setup_dhcp_config() {
+
+    sudo_required
+
+    cat >> /etc/dhcpcd.conf <<-EOF
+# --- added by setup_env.sh
+interface eth0
+static ip_address=192.168.0.1/24
+#static routers=192.168.0.1
+#static domain_name_servers=192.168.0.1
+static domain_search=
+# --- end setup_env.sh
+	EOF
+
+    sudo -s <<-EOF
+echo "# --- added by setup_env.sh
+#DHCP server active for eth0 interface
+interface=eth0
+
+#DHCP server not active for wlan0
+no-dhcp-interface = wlan0
+
+# Ip range
+dhcp-range=192.168.0.100,192.168.0.150,24h
+# --- end setup_env.sh" > /etc/dnsmasq.conf
+	EOF
+}
+
+#----------------------------------#
+# Main section                     #
+#----------------------------------#
+
+if [ `id -u` == "0" ]
+then
+	echo_red "This script should not be run as root" 1>&2
+	exit 1
+fi
+
+pushd $SCRIPT_DIR
+
+STEPS="bashrc_update disable_sudo_passwd misc_profile_cleanup raspi_config xfce4_power_manager_settings"
+STEPS="$STEPS thunar_volman disable_lxde_automount apt_install_prereqs"
+STEPS="$STEPS write_autostart_config libiio pyadi-iio adm1266"
+STEPS="$STEPS pi_boot_config disable_pi_screen_blanking"
+STEPS="$STEPS dhcp_config"
+
+RAN_ONCE=0
+for step in $STEPS ; do
+	setup_$step
+done
+
+popd
